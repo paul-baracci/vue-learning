c321da43769fd46034fed2aabc7e4aa5575a01e1
Paul Paracci <baracci.paul@gmail.com>
2021-06-28 01:04:43 +0300

    Added logic for card count, price, total, add/remove buttons

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -5,17 +5,19 @@
         <div class="row" v-for="(product, i) in products" :key="i" :product="product">
             <div class="col">{{product.name}}</div>
             <div class="col">Price: ${{product.price}}</div>
-            <button class="col btn btn-success" @click="addToCart(product)">Add to cart</button>
+            <button class="col-2 btn btn-success" @click="addToCart(product)">Add to cart</button>
         </div>
     </div>
         <div class="text-center">
-        <h1>Cart items {{cart.length}}</h1>
+        <h1>Cart ({{cart.length}})</h1>
         <div class="row" v-for="product in cart" :key="product.id" :item="product">
         <div class="col">{{product.name}}</div>
-        <div class="col">Price: ${{product.price}}</div>
         <div class="col">Count: {{product.count}}</div>
-        <button class="col btn btn-danger" @click="removeFromCart(product)">x</button>
+        <div class="col">Price: ${{product.total}}</div>
+        <button class="col-1 btn btn-success" @click="addToCart(product)">+</button>
+        <button class="col-1 btn btn-danger" @click="decreaseCartItem(product)">-</button>
         </div>
+        <div class="col">Total: ${{total}}</div>
     </div>
 </div> 
 </template>
@@ -23,6 +25,22 @@
 <script>
 
 export default {
+  computed: {
+    singleCost () {
+      let cost = 0;
+      this.cart.forEach((i) => {
+        cost = i.price * i.count;
+      });
+      return cost;
+    },
+    total () {
+      let totalCost = 0
+      for (let i in this.cart) {
+        totalCost += this.cart[i].price * this.cart[i].count
+      }
+      return totalCost;
+    },
+  },
   data() {
     return {
       cart: [],
@@ -47,13 +65,20 @@ export default {
       let found = this.cart.some(item => item.id === product.id)
         if(found == 0) {
           this.cart.push(product);
+          product.total = product.price
         } else {
           product.count++
+          product.total = product.price * product.count
         }
     },
-    removeFromCart(product) {
-      this.cart.splice(this.cart.indexOf(product), 1);
-    },
+    decreaseCartItem(product) {
+      if (product.count == 1) {
+        this.cart.splice(this.cart.indexOf(product), 1);
+      } else {
+      product.count--
+      product.total = product.price * product.count
+      }
+    }
   }
 }
 </script>
